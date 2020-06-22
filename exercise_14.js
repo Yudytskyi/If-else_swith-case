7ecf1ce93f8bf55619da03b24737c640f6d0e447
Yudytskyi <uan0503631989@gmail.com>
2020-06-22 11:27:40 +0300

    add exercise_14.js

---
+++ exercise_14.js
@@ -0,0 +1,5 @@
+let a = Number(prompt('Please enter your value a='));
+let b = Number(prompt('Please enter your value b='));
+let result;
+
+a > 2 && a < 11 || b >= 6 && b < 14 ? alert(true) : alert(false);
