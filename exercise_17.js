e57791edbc3c0cc523a6dad74b1db9559b07a2ca
Yudytskyi <uan0503631989@gmail.com>
2020-06-22 15:46:37 +0300

    error removal in exercise_17.js

--- exercise_17.js
+++ exercise_17.js
@@ -24,7 +24,7 @@ let sum1 = 0;
 let sum2 = 0;
 
 for (i = 0; i < string.length; i++) {
-    i < (string.length - 1) / 2 ? sum1 += Number(string[i]) : sum2 += Number(string[i]);
+    i < string.length / 2 ? sum1 += Number(string[i]) : sum2 += Number(string[i]);
 }
 
 sum1 === sum2 ? alert(true) : alert(false);
