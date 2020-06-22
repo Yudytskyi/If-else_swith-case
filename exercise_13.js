9bac9da6490ad2fd36eada4d21dd8e79f4b31677
Yudytskyi <uan0503631989@gmail.com>
2020-06-22 11:21:41 +0300

    add exercise_13.js

---
+++ exercise_13.js
@@ -0,0 +1,6 @@
+let a = Number(prompt('Please enter your value a='));
+let b = Number(prompt('Please enter your value b='));
+let result;
+
+a <= 1 && b >= 3 ? result = a + b : result = a - b;
+alert(result)
